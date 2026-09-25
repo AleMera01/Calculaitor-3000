13f94ac1deec68ceaf8ab97555c0874c05002cf5
AleMera01 <amera@farmaciasantamaria.com>
2026-09-25 08:20:23 +0200

    Add files via upload
    actualizar v.3

---
+++ calculadoras/castilla-la-mancha/js/pretasadas.js
@@ -0,0 +1,696 @@
+const preparadasCLM = {
+
+  "140002": {
+    nombre: "CAPTOPRIL 4MG/ML 100ML 1 FRASCO SUSPENSION ORAL (FM) - (CAPTOPRIL)",
+    principios: 6.128,
+    excipientes: 0.83,
+    envase: 0.88,
+    honorarios: 7.3035,
+    total: 15.74716
+  },
+
+  "140003": {
+    nombre: "COBRE SULFATO 10MG/ML 250ML 1 FRASCO SOLUCION CUTANEA (FM) - (COBRE SULFATO)",
+    principios: 0.20,
+    excipientes: 2.49,
+    envase: 0.92,
+    honorarios: 9.129375,
+    total: 13.24895
+  },
+
+  "140005": {
+    nombre: "OMEPRAZOL 2 MG/ML JARABE 100 ML FM - (OMEPRAZOL)",
+    principios: 0.53,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 11.79724
+  },
+
+  "140007": {
+    nombre: "TRIAMCINOLONA ACETONIDO 1MG/G 100G 1 ENVASE EMULSION CUTANEA O/A (FM) - (TRIAMCINOLONA)",
+    principios: 1.113,
+    excipientes: 2.60,
+    envase: 0.93,
+    honorarios: 9.738,
+    total: 14.95624
+  },
+
+  "140008": {
+    nombre: "TRIAMCINOLONA ACETONIDO 1MG/G 150G 1 ENVASE EMULSION CUTANEA O/A (FM) - (TRIAMCINOLONA)",
+    principios: 1.6695,
+    excipientes: 5.20,
+    envase: 1.11,
+    honorarios: 11.320425,
+    total: 20.071922
+  },
+
+  "140011": {
+    nombre: "ATENOLOL 2MG/ML 100ML 1 FRASCO SUSPENSION ORAL (FM) - (ATENOLOL)",
+    principios: 0.484,
+    excipientes: 0.83,
+    envase: 0.88,
+    honorarios: 7.3035,
+    total: 9.87740
+  },
+
+  "140012": {
+    nombre: "BETAMETASONA DIPROPIONATO 1MG/G 100G 1 ENVAS EMULSION CUTANEA O/A (FM) - (BETAMETASONA)",
+    principios: 1.26,
+    excipientes: 2.60,
+    envase: 0.93,
+    honorarios: 9.738,
+    total: 15.10912
+  },
+
+  "140020": {
+    nombre: "CAPTOPRIL 1 MG/ML JARABE 100 ML 1 FRASCO FM - (CAPTOPRIL)",
+    principios: 1.532,
+    excipientes: 2.60,
+    envase: 0.88,
+    honorarios: 7.3035,
+    total: 12.80812
+  },
+
+  "140021": {
+    nombre: "CAPTOPRIL 1 MG/ML JARABE 150 ML 1 FRASCO FM - (CAPTOPRIL)",
+    principios: 2.298,
+    excipientes: 5.20,
+    envase: 0.92,
+    honorarios: 7.912125,
+    total: 16.98333
+  },
+
+  "140022": {
+    nombre: "CAPTOPRIL 1 MG/ML JARABE 200 ML 1 FRASCO FM - (CAPTOPRIL)",
+    principios: 3.064,
+    excipientes: 5.20,
+    envase: 0.92,
+    honorarios: 8.52075,
+    total: 18.41294
+  },
+
+  "140023": {
+    nombre: "CARBAMAZEPINA 40 MG/ML JARABE 100 ML 1 FRASCO FM - (CARBAMAZEPINA)",
+    principios: 2.24,
+    excipientes: 2.60,
+    envase: 0.88,
+    honorarios: 7.3035,
+    total: 13.54444
+  },
+
+  "140029": {
+    nombre: "COBRE SULFATO 4G 25 SOBRES POLVO SOLUCION CUTANEA (FM) - (COBRE SULFATO)",
+    principios: 8.00,
+    excipientes: 0.00,
+    envase: 0.87,
+    honorarios: 10.95525,
+    total: 20.61826
+  },
+
+  "140030": {
+    nombre: "COBRE SULFATO 1 MG/ML 100 ML FRASCO SOLUCION CUTANEA",
+    principios: 0.01,
+    excipientes: 0.83,
+    envase: 0.88,
+    honorarios: 7.30,
+    total: 9.38
+  },
+
+  "140031": {
+    nombre: "Cobre sulfato 1 mg/ml 250 ml solución cutánea",
+    principios: 0.02,
+    excipientes: 2.49,
+    envase: 0.92,
+    honorarios: 9.129375,
+    total: 13.06175
+  },
+
+  "140033": {
+    nombre: "Diltiazem 20 mg/g gel 100 g",
+    principios: 5.60,
+    excipientes: 2.60,
+    envase: 0.93,
+    honorarios: 9.738,
+    total: 19.62272
+  },
+
+  "140034": {
+    nombre: "Diltiazem 20 mg/g gel 30 g",
+    principios: 1.68,
+    excipientes: 2.60,
+    envase: 0.80,
+    honorarios: 9.738,
+    total: 15.41072
+  },
+  "140035": {
+    nombre: "DILTIAZEM 20 MG/G POMADA 60 G 1 ENVASE FM - (DILTIAZEM)",
+    principios: 3.36,
+    excipientes: 1.86,
+    envase: 0.80,
+    honorarios: 9.738,
+    total: 16.38832
+  },
+
+  "140036": {
+    nombre: "ENALAPRIL 1 MG/ML JARABE 100 ML 1 FRASCO FM - (ENALAPRIL)",
+    principios: 0.487,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 11.75252
+  },
+
+  "140038": {
+    nombre: "ESPIRONOLACTONA 5 MG/ML 125 ML 1 FRASCO SUSPENSION ORAL (FM) - (ESPIRONOLACTONA)",
+    principios: 2.45,
+    excipientes: 1.66,
+    envase: 0.91,
+    honorarios: 7.7295375,
+    total: 13.259519
+  },
+
+  "140044": {
+    nombre: "FUROSEMIDA 1 MG/ML JARABE 180 ML 1 FRASCO FM - (FUROSEMIDA)",
+    principios: 0.3726,
+    excipientes: 5.20,
+    envase: 1.33,
+    honorarios: 8.2773,
+    total: 15.787096
+  },
+
+  "140045": {
+    nombre: "FUROSEMIDA 2 MG/ML JARABE 100 ML 1 FRASCO FM - (FUROSEMIDA)",
+    principios: 0.414,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 11.67660
+  },
+
+  "140046": {
+    nombre: "FUROSEMIDA 2 MG/ML JARABE 60 ML 1 FRASCO FM - (FUROSEMIDA)",
+    principios: 0.2484,
+    excipientes: 2.60,
+    envase: 0.80,
+    honorarios: 7.3035,
+    total: 11.389976
+  },
+
+  "140047": {
+    nombre: "HIDROCORTISONA 1 MG/ML JARABE 120 ML 1 FRASCO FM - (HIDROCORTISONA)",
+    principios: 0.8832,
+    excipientes: 5.20,
+    envase: 0.91,
+    honorarios: 7.54695,
+    total: 15.121756
+  },
+
+  "140054": {
+    nombre: "METRONIDAZOL 20 MG/G GEL 200 G 1 ENVASE FM - (METRONIDAZOL)",
+    principios: 2.24,
+    excipientes: 5.20,
+    envase: 1.11,
+    honorarios: 12.90285,
+    total: 22.310964
+  },
+
+  "140055": {
+    nombre: "METRONIDAZOL 40 MG/G CREMA O/A 250 G 1 ENVASE FM - (METRONIDAZOL)",
+    principios: 5.60,
+    excipientes: 7.80,
+    envase: 1.11,
+    honorarios: 14.485275,
+    total: 30.155086
+  },
+
+  "140056": {
+    nombre: "METRONIDAZOL/LIDOCAINA (20 MG + 10 MG)/G 200 G 1 ENVASE GEL CUTANEO (FM) - (METRONIDAZOL + LIDOCAINA)",
+    principios: 2.534,
+    excipientes: 5.20,
+    envase: 1.11,
+    honorarios: 12.90285,
+    total: 22.616724
+  },
+
+  "140060": {
+    nombre: "NIFEDIPINO 3 MG/G POMADA 100 G 1 ENVASE FM - (NIFEDIPINO)",
+    principios: 0.987,
+    excipientes: 1.86,
+    envase: 0.93,
+    honorarios: 9.738,
+    total: 14.05560
+  },
+
+  "140062": {
+    nombre: "OMEPRAZOL 2 MG/ML JARABE 200 ML 1 FRASCO FM - (OMEPRAZOL)",
+    principios: 1.06,
+    excipientes: 5.20,
+    envase: 1.33,
+    honorarios: 8.52075,
+    total: 16.75518
+  },
+
+  "140063": {
+    nombre: "OMEPRAZOL 2 MG/ML JARABE 60 ML 1 FRASCO FM - (OMEPRAZOL)",
+    principios: 0.318,
+    excipientes: 2.60,
+    envase: 0.80,
+    honorarios: 7.3035,
+    total: 11.46236
+  },
+
+  "140064": {
+    nombre: "OMEPRAZOL 4 MG/ML JARABE 100 ML 1 FRASCO FM - (OMEPRAZOL)",
+    principios: 1.06,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 12.34844
+  },
+
+  "140065": {
+    nombre: "OMEPRAZOL 4 MG/ML JARABE 250 ML 1 FRASCO FM - (OMEPRAZOL)",
+    principios: 2.65,
+    excipientes: 7.80,
+    envase: 1.33,
+    honorarios: 9.129375,
+    total: 21.74575
+  },
+
+  "140070": {
+    nombre: "PREDNISONA 10 MG/ML JARABE 125 ML 1 FRASCO FM - (PREDNISONA)",
+    principios: 6.3125,
+    excipientes: 5.20,
+    envase: 0.91,
+    honorarios: 7.7295375,
+    total: 20.958119
+  },
+
+  "140071": {
+    nombre: "PROPRANOLOL 2 MG/ML JARABE 100 ML 1 FRASCO FM - (PROPRANOLOL)",
+    principios: 0.108,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 11.35836
+  },
+  "140072": {
+    nombre: "PROPRANOLOL 5 MG/ML JARABE 250 ML 1 FRASCO FM - (PROPRANOLOL)",
+    principios: 0.675,
+    excipientes: 7.80,
+    envase: 1.33,
+    honorarios: 9.129375,
+    total: 19.69175
+  },
+
+  "140083": {
+    nombre: "RIBOFLAVINA 100MG 100 CAPSULAS DURAS (FM) - (RIBOFLAVINA (VITAMINA B2))",
+    principios: 3.60,
+    excipientes: 1.00,
+    envase: 0.94,
+    honorarios: 31.6485,
+    total: 38.67604
+  },
+
+  "140084": {
+    nombre: "RIBOFLAVINA 10 MG/ML JARABE 250 ML 1 FRASCO FM - (RIBOFLAVINA (VITAMINA B2))",
+    principios: 0.90,
+    excipientes: 7.80,
+    envase: 1.33,
+    honorarios: 9.129375,
+    total: 19.92575
+  },
+
+  "140105": {
+    nombre: "TESTOSTERONA PROPIONATO 20 MG/G POMADA EN VASELINA 100 G 1 ENVASE FM - (TESTOSTERONA)",
+    principios: 15.86,
+    excipientes: 1.86,
+    envase: 0.93,
+    honorarios: 9.738,
+    total: 29.52352
+  },
+
+  "140108": {
+    nombre: "TRIAMCINOLONA ACETONIDO 1MG/G 250G 1 ENVASE POMADA CUTANEA (FM) - (TRIAMCINOLONA)",
+    principios: 2.7825,
+    excipientes: 5.58,
+    envase: 1.11,
+    honorarios: 13.38975,
+    total: 23.77674
+  },
+
+  "140113": {
+    nombre: "TRIAMCINOLONA ACET/SALICILICO (1MG+30MG)/G 100G EMULS CUTANEA O/A (FM) - (SALICILICO ACIDO + TRIAMCINOLONA ACETONIDO)",
+    principios: 1.209,
+    excipientes: 2.60,
+    envase: 0.93,
+    honorarios: 9.738,
+    total: 15.05608
+  },
+
+  "140115": {
+    nombre: "TRIAMCINOLONA ACET/SALICILICO (0,1MG+30MG)/G 100G EMULSION CUTANEA O/A (FM) - (SALICILICO ACIDO + TRIAMCINOLONA ACETONIDO)",
+    principios: 0.2073,
+    excipientes: 2.60,
+    envase: 0.93,
+    honorarios: 9.738,
+    total: 14.014312
+  },
+
+  "140117": {
+    nombre: "ACIDO URSODEOXICOLICO 20MG/ML 100ML 1 FRASCO JARABE (FM) - (URSODEOXICOLICO ACIDO)",
+    principios: 6.36,
+    excipientes: 2.60,
+    envase: 0.88,
+    honorarios: 7.3035,
+    total: 17.82924
+  },
+
+  "140129": {
+    nombre: "OMEPRAZOL 2MG/ML 100ML 1 FRASCO SUSPENSION ORAL (FM) - (OMEPRAZOL)",
+    principios: 0.53,
+    excipientes: 0.83,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 9.95644
+  },
+
+  "140130": {
+    nombre: "OMEPRAZOL 4MG/ML 100ML 1 FRASCO SUSPENSION ORAL (FM) - (OMEPRAZOL)",
+    principios: 1.06,
+    excipientes: 0.83,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 10.50764
+  },
+
+  "149001": {
+    nombre: "PIRIDOXINA CLORHIDRATO 25 MG/ML 100 ML JARABE (FM) - (PIRIDOXINA)",
+    principios: 0.700,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.300,
+    total: 11.9704
+  },
+
+  "149002": {
+    nombre: "PIRIDOXINA CLORHIDRATO 25 MG/ML 50 ML JARABE (FM) - (PIRIDOXINA)",
+    principios: 0.350,
+    excipientes: 2.60,
+    envase: 0.80,
+    honorarios: 7.300,
+    total: 11.4920
+  },
+
+  "149003": {
+    nombre: "ÁCIDO FÓLICO 1 MG/ML 100 ML 1 FRASCO SOLUCIÓN ORAL (FM) - (ÁCIDO FÓLICO)",
+    principios: 0.2047,
+    excipientes: 0.83,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 9.618128
+  },
+
+  "149004": {
+    nombre: "ÁCIDO FÓLICO 1 MG/ML 50 ML 1 FRASCO SOLUCIÓN ORAL (FM) - (ÁCIDO FÓLICO)",
+    principios: 0.10235,
+    excipientes: 0.83,
+    envase: 0.80,
+    honorarios: 7.3035,
+    total: 9.397284
+  },
+
+  "149005": {
+    nombre: "HIDROCLOROTIAZIDA 2 MG/ML 100 ML 1 FRASCO SUSPENSIÓN ORAL (FM) - (HIDROCLOROTIAZIDA)",
+    principios: 0.164,
+    excipientes: 0.83,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 9.5758
+  },
+
+  "149006": {
+    nombre: "HIDROCLOROTIAZIDA 2 MG/ML 50 ML 1 FRASCO SUSPENSIÓN ORAL (FM) - (HIDROCLOROTIAZIDA)",
+    principios: 0.082,
+    excipientes: 0.83,
+    envase: 0.80,
+    honorarios: 7.3035,
+    total: 9.37612
+  },
+
+  "149007": {
+    nombre: "FUROSEMIDA 2 MG/ML 100 ML 1 FRASCO SOLUCIÓN ORAL (FM) - (FUROSEMIDA)",
+    principios: 0.414,
+    excipientes: 0.83,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 9.8358
+  },
+
+  "149008": {
+    nombre: "FUROSEMIDA 2 MG/ML 50 ML 1 FRASCO SOLUCIÓN ORAL (FM) - (FUROSEMIDA)",
+    principios: 0.207,
+    excipientes: 0.83,
+    envase: 0.80,
+    honorarios: 7.3035,
+    total: 9.50612
+  },
+
+  "149009": {
+    nombre: "CLONIDINA CLORHIDRATO 20 MICROG/ML 100 ML JARABE (FM) - (CLONIDINA)",
+    principios: 0.0242,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 11.271208
+  },
+
+  "149010": {
+    nombre: "CLONIDINA CLORHIDRATO 20 MICROG/ML 50 ML 1 JARABE (FM) - (CLONIDINA)",
+    principios: 0.0121,
+    excipientes: 2.60,
+    envase: 0.80,
+    honorarios: 7.3035,
+    total: 11.144224
+  },
+
+  "149011": {
+    nombre: "ISONIAZIDA 50 MG/ML 100 ML 1 FRASCO SOLUCIÓN ORAL (FM) - (ISONIAZIDA)",
+    principios: 1.95,
+    excipientes: 0.83,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 11.43324
+  },
+
+  "149012": {
+    nombre: "ISONIAZIDA 50 MG/ML 50 ML 1 FRASCO SOLUCIÓN ORAL (FM) - (ISONIAZIDA)",
+    principios: 0.975,
+    excipientes: 0.83,
+    envase: 0.80,
+    honorarios: 7.3035,
+    total: 10.30484
+  },
+
+  "149013": {
+    nombre: "DEXAMETASONA 1 MG/ML 100 ML JARABE (FM) - (DEXAMETASONA)",
+    principios: 1.33,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 12.62924
+  },
+
+  "149014": {
+    nombre: "DEXAMETASONA 1 MG/ML 50 ML JARABE (FM) - (DEXAMETASONA)",
+    principios: 0.665,
+    excipientes: 2.60,
+    envase: 0.80,
+    honorarios: 7.3035,
+    total: 11.82324
+  },
+
+  "149015": {
+    nombre: "FENOBARBITAL 10 MG/ML 100 ML 1 FRASCO SOLUCIÓN ORAL (FM) - (FENOBARBITAL)",
+    principios: 3.64,
+    excipientes: 0.83,
+    envase: 0.91,
+    honorarios: 7.3035,
+    total: 13.19084
+  },
+
+  "149016": {
+    nombre: "FENOBARBITAL 10 MG/ML 50 ML 1 FRASCO SOLUCIÓN ORAL (FM) - (FENOBARBITAL)",
+    principios: 1.82,
+    excipientes: 0.83,
+    envase: 0.80,
+    honorarios: 7.3035,
+    total: 11.18364
+  },
+
+  "149017": {
+    nombre: "HIDRALAZINA CLORHIDRATO 10 MG/ML 100 ML 1 FRASCO SOLUCIÓN ORAL (FM) - (HIDRALAZINA)",
+    principios: 6.89,
+    excipientes: 0.83,
+    envase: 0.91,
+    honorarios: 7.30,
+    total: 16.56720
+  },
+
+  "149018": {
+    nombre: "HIDRALAZINA CLORHIDRATO 10 MG/ML 50 ML 1 FRASCO SOLUCIÓN ORAL (FM) - (HIDRALAZINA)",
+    principios: 3.445,
+    excipientes: 0.83,
+    envase: 0.80,
+    honorarios: 7.30,
+    total: 12.87000
+  },
+
+  "149019": {
+    nombre: "RIBOFLAVINA 25 MG/ML 100 ML JARABE (FM) - (RIBOFLAVINA)",
+    principios: 0.90,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.30,
+    total: 12.17840
+  },
+
+  "149020": {
+    nombre: "RIBOFLAVINA 25 MG/ML 50 ML JARABE (FM) - (RIBOFLAVINA)",
+    principios: 0.45,
+    excipientes: 2.60,
+    envase: 0.80,
+    honorarios: 7.30,
+    total: 11.59600
+  },
+
+  "149021": {
+    nombre: "ENALAPRIL MALEATO 1 MG/ML 50 ML JARABE (FM) - (ENALAPRIL)",
+    principios: 0.24,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.30,
+    total: 11.38
+
+  },
+
+  "149022": {
+    nombre: "AMLODIPINO 0,5 MG/ML 100 ML JARABE (FM) - (AMLODIPINO)",
+    principios: 1.519,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.30,
+    total: 12.82216
+  },
+
+  "149023": {
+    nombre: "AMLODIPINO 0,5 MG/ML 50 ML JARABE (FM) - (AMLODIPINO)",
+    principios: 0.7595,
+    excipientes: 2.60,
+    envase: 0.80,
+    honorarios: 7.30,
+    total: 11.91788
+  },
+
+  "149024": {
+    nombre: "HIDROCORTISONA 1 MG/ML 100 ML JARABE (FM) - (HIDROCORTISONA)",
+    principios: 0.736,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.30,
+    total: 12.00784
+  },
+
+  "149025": {
+    nombre: "HIDROCORTISONA 1 MG/ML 50 ML JARABE (FM) - (HIDROCORTISONA)",
+    principios: 0.368,
+    excipientes: 2.60,
+    envase: 0.80,
+    honorarios: 7.30,
+    total: 11.51072
+  },
+
+  "149026": {
+    nombre: "GABAPENTINA 50 MG/ML 100 ML JARABE (FM) - (GABAPENTINA)",
+    principios: 8.25,
+    excipientes: 2.60,
+    envase: 0.91,
+    honorarios: 7.30,
+    total: 19.82240
+  },
+
+  "149027": {
+    nombre: "GABAPENTINA 50 MG/ML 50 ML JARABE (FM) - (GABAPENTINA)",
+    principios: 4.125,
+    excipientes: 2.60,
+    envase: 0.80,
+    honorarios: 7.30,
+    total: 15.41800
+  },
+
+  "140109": {
+    nombre: "TRIAMCINOLONA, ACETONIDO 1 MG/G EN ORABASE, 100 G (FM)",
+    principios: 1.1132,
+    excipientes: 1.86,
+    envase: 0.93,
+    honorarios: 9.738,
+    total: 14.186848
+  },
+
+  "140111": {
+    nombre: "TRIAMCINOLONA, ACETONIDO 2 MG/G EN ORABASE, 50 G (FM)",
+    principios: 1.1132,
+    excipientes: 1.86,
+    envase: 0.80,
+    honorarios: 9.738,
+    total: 14.051648
+  },
+
+  "149000": {
+    nombre: "CLOBETASOL PROPIONATO 25 MG/G EN ORABASE, 50 G (FM)",
+    principios: 12.00925,
+    excipientes: 1.86,
+    envase: 0.80,
+    honorarios: 9.738,
+    total: 25.38354
+  },
+
+"149030": {
+    nombre: "CROMOGLICATO 40 MG/G CREMA 100 G",
+    principios: 0,
+    excipientes: 0,
+    envase: 0,
+    honorarios: 0,
+    total: 25.53
+  },
+
+  "149028": {
+    nombre: "CROMOGLICATO DISODICO 2MG/G CREMA 200 G",
+    principios: 0,
+    excipientes: 0,
+    envase: 0,
+    honorarios: 0,
+    total: 21.15
+  },
+
+  "149029": {
+    nombre: "CROMOGLICATO DISODICO 40 MG/G EMULSION O/W 250 G",
+    principios: 0,
+    excipientes: 0,
+    envase: 0,
+    honorarios: 0,
+    total: 53.65
+  },
+
+  "149031": {
+    nombre: "CROMOGLICATO SODICO 25 MG 90 CAPSULAS",
+    principios: 0,
+    excipientes: 0,
+    envase: 0,
+    honorarios: 0,
+    total: 38.89
+  }
+
+};
+
--- sw.js
+++ sw.js
@@ -1,57 +1,123 @@
 const CACHE_NAME = "calculaitor-3000-v2";
 
+
+/* =========================
+   INSTALACIÓN
+   ========================= */
+
 self.addEventListener("install", event => {
+
     self.skipWaiting();
+
 });
+
+
+/* =========================
+   ACTIVACIÓN
+   ========================= */
 
 self.addEventListener("activate", event => {
+
     event.waitUntil(
+
         caches.keys().then(cacheNames => {
+
             return Promise.all(
+
                 cacheNames.map(cacheName => {
+
                     if (cacheName !== CACHE_NAME) {
+
                         return caches.delete(cacheName);
+
                     }
+
                 })
+
             );
+
         }).then(() => {
+
             return self.clients.claim();
+
         })
+
     );
+
 });
 
+
+/* =========================
+   PETICIONES
+   ========================= */
+
 self.addEventListener("fetch", event => {
+
     if (event.request.method !== "GET") {
+
         return;
+
     }
 
+
     event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-            if (cachedResponse) {
-                return cachedResponse;
-            }
 
-            return fetch(event.request)
-                .then(networkResponse => {
-                    if (
-                        !networkResponse ||
-                        networkResponse.status !== 200 ||
-                        networkResponse.type === "opaque"
-                    ) {
+        caches.match(event.request)
+
+            .then(cachedResponse => {
+
+                if (cachedResponse) {
+
+                    return cachedResponse;
+
+                }
+
+
+                return fetch(event.request)
+
+                    .then(networkResponse => {
+
+                        if (
+                            !networkResponse ||
+                            networkResponse.status !== 200 ||
+                            networkResponse.type === "opaque"
+                        ) {
+
+                            return networkResponse;
+
+                        }
+
+
+                        const responseToCache =
+                            networkResponse.clone();
+
+
+                        caches.open(CACHE_NAME)
+
+                            .then(cache => {
+
+                                cache.put(
+                                    event.request,
+                                    responseToCache
+                                );
+
+                            });
+
+
                         return networkResponse;
-                    }
 
-                    const responseToCache = networkResponse.clone();
+                    })
 
-                    caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request, responseToCache);
+                    .catch(() => {
+
+                        return caches.match(
+                            "./index.html"
+                        );
+
                     });
 
-                    return networkResponse;
-                })
-                .catch(() => {
-                    return caches.match("./index.html");
-                });
-        })
+            })
+
     );
+
 });
